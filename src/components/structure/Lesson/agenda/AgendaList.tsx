10d1bcd4468fefcf33f29b2c21b1eae9dd34b13b
Dmitriy Lakhno <dimalahno@rambler.ru>
2025-12-04 12:56:47 +0500

    Wrap lesson plan in accordion

--- src/components/structure/Lesson/agenda/AgendaList.tsx
+++ src/components/structure/Lesson/agenda/AgendaList.tsx
@@ -1,21 +1,32 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import type { AgendaEntry } from "../../../../types/content";
 
 export type AgendaListEntry = AgendaEntry & { name: string };
 
 export function AgendaList({ entries }: { entries: AgendaListEntry[] }) {
     return (
-        <>
-            <Typography variant="h6" gutterBottom>
-                План урока
-            </Typography>
-            <List>
-                {entries.map((item) => (
-                    <ListItem key={item.name} divider>
-                        <ListItemText primary={`${item.order_id}. ${item.name}`} secondary={item.description} />
-                    </ListItem>
-                ))}
-            </List>
-        </>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="h6">План урока</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <List>
+                    {entries.map((item) => (
+                        <ListItem key={item.name} divider>
+                            <ListItemText primary={`${item.order_id}. ${item.name}`} secondary={item.description} />
+                        </ListItem>
+                    ))}
+                </List>
+            </AccordionDetails>
+        </Accordion>
     );
 }
